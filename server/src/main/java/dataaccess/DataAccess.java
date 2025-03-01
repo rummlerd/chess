a98d88cfdb32f7a493c4d28620e7d84c88933508
rummlerd <rummlerd@byu.edu>
2025-03-01 14:44:22 -0700

    Implemented DataAccess and MemoryDataAccess in preparation of implementing Registration Requests from the client

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,5 +1,39 @@
 package dataaccess;
 
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
 public interface DataAccess {
-    //FIXME add handlers here
+    /**
+     * Clear all users, games, and authTokens from the database, for use in testing
+     */
+    void clear();
+
+    /**
+     * Creates a new user. Throws a 403 already taken error if user already exists
+     * @param user
+     */
+    void createUser(UserData user) throws IllegalArgumentException;
+
+    /**
+     * Returns UserData if user exists, returns null if no user exists
+     * @param username
+     * @return UserData
+     */
+    UserData getUser(String username);
+
+    /**
+     * Create a new authorization. Call after every login or registration. Returns an authToken which can be sent to the client for further actions
+     * @param username
+     * @return authToken
+     */
+    String createAuth(String username) throws IllegalArgumentException;
+
+    /**
+     * Retrieve an authorization given an authToken, throws an 401 unauthorized error if authToken not found
+     * @param authToken
+     * @return AuthData
+     */
+    AuthData getAuth(String authToken);
 }
--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,5 +1,60 @@
 package dataaccess;
 
+import java.util.*;
+import java.util.UUID;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
 public class MemoryDataAccess implements DataAccess {
-    //FIXME
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+    private final Map<String, GameData> games = new HashMap<>();
+
+
+    @Override
+    public void clear() {
+        users.clear();
+        authTokens.clear();
+        games.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) {
+        if (users.containsKey(user.username())) {
+            throw new IllegalArgumentException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        UserData user = users.get(username);
+        if (user == null) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        return user;
+    }
+
+    @Override
+    public String createAuth(String username) {
+        if (!users.containsKey(username)) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+
+        String authToken = UUID.randomUUID().toString();
+        authTokens.put(authToken, new AuthData(authToken, username));
+
+        return authToken;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        AuthData auth = authTokens.get(authToken);
+        if (auth == null) {
+            throw new IllegalArgumentException("Unauthorized");
+        }
+        return auth;
+    }
 }
